14a78f460b7149245b32a5ee1e94291e6be5762e
Jaedoo <dlwoen9@godo.co.kr>
2021-05-02 19:06:19 +0900

    feat: 상수 추가

--- src/main/java/com.study/domain/GameConstants.java
+++ src/main/java/com.study/domain/GameConstants.java
@@ -6,6 +6,6 @@ package com.study.domain;
 public class GameConstants {
     public static String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static String PLAY_COUNT = "시도할 회수는 몇회인가요?";
-    public static String PLAY_RESULT = "실행결과";
+    public static String PLAY_RESULT = "\n실행결과";
     public static String WINNER = "가 최종 우승했습니다.";
 }
